e77108bc28bd814d7ece52b1a3f64d112a2459c9
unknown <JREAST@ANC-W7854201152.conoco.net>
2014-11-22 17:21:37 -0900

    Final version for submission Added more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,15 @@
-## These functions calculate the inverse of a matrix, cache it, and retrieve it
-## if the inverse of the same matrix is requested again
-##   makeCacheMatrix - Creates matrix object and get/set methods
-##   cacheSolve - Calculates and caches inverse, or retrieves cached inverse
+# Functions to calculate the inverse of a matrix, cache it, and retrieve it
+#  if the inverse of the same matrix is requested again
+#     Functions - makeCacheMatrix, cacheSolve
 
-## makeCacheMatrix - Function to create matrix object and get/set methods
+# Function makeCacheMatrix - Create matrix object and get/set methods
+#   Arguments - Matrix to turn into a matrix object
+#   Returns -   List of methods for matrix object (functions)
 
 makeCacheMatrix <- function(xMat = matrix()) {
     cachedInv <- NULL  # Will be used to store cached inverse
     
-    # Create set function 
-    #   Purpose   - Changes input matrix for an array object that already exists
+    # Function set - Change value of input matrix for an existing matrix object
     #   Arguments - New input matrix
     #   Returns -   not used
     set <- function (yMat) {
@@ -17,29 +17,27 @@ makeCacheMatrix <- function(xMat = matrix()) {
         cachedInv <<- NULL
     }
 
-    # Create get function 
-    #   Purpose   - Retrieve input matrix for makeCacheMatrix object
+    # Function get - Retrieve input matrix for matrix object
     #   Arguments - None
     #   Returns -   Matrix passed as an argument to makeCacheMatrix when it was
-    #               called to create the object with this function as a method
+    #                called to create the object with this function as a method
     get <- function () { xMat }  
     
-    # Create setmatinv function 
-    #   Purpose   - Cache calculated inverse matrix for object (superassigment)
+    # Function setmatinv - Cache calculated inverse matrix (superassigment)
     #   Arguments - Inverse matrix to be stored (Really any data type)
     #   Returns -   not used
     setmatinv <- function(xMatInv) {
         cachedInv <<- xMatInv
     }
    
-    # Create getmatinv function 
-    #   Purpose   - Retrieve cached inverse matrix for object
+    # Function getmatinv - Retrieve cached inverse matrix for object
     #   Arguments - None
-    #   Returns -   Cached inverse matrix (saved by earlier cal to setmatinv)
+    #   Returns -   Cached inverse matrix (saved by earlier call to setmatinv)
     getmatinv <- function() {
         cachedInv
     }
     
+    # Return a list with the four methods that have been created 
     list( set=set, 
           get = get, 
           setmatinv = setmatinv,
@@ -47,17 +45,24 @@ makeCacheMatrix <- function(xMat = matrix()) {
 }
 
 
-## Write a short comment describing this function
+# makeCacheMatrix - Calculates/caches inverse, or retrieves cached inverse
+#   Arguments - Matrix object (from makeCacheMatrix) to invert
+#   Returns -   Inverse matrix
 
 cacheSolve <- function(xObj, ...) {
+    # Check if there is a cached inverse for object
     matInv <- xObj$getmatinv()
     if (!is.null(matInv)) {
         message("Found cached inverse")
-        return(matInv)
     }
-    matRaw <- xObj$get()
-    matInv <- solve(matRaw)
-    xObj$setmatinv(matInv)
-    matInv
     
+    # If not, retrieve matrix for this object, invert it and cache the result
+    else {
+        matRaw <- xObj$get()
+        matInv <- solve(matRaw)
+        xObj$setmatinv(matInv)
+    }
+    
+    # Return the inverse matrix
+    matInv
 }
